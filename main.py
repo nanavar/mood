ffe3215d6de6ee6de21df89fca4478cdf42ab9ae
Ana Vargazon <ana.vargazon@gmail.com>
2021-10-11 11:38:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood today? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times and think about your happy place.")
+elif mood == "sad":
+    print("Cheer up mate!")
+elif mood == "excited":
+    print("Yay! Me too!")
+elif mood == "relaxed":
+    print("Let's drink a warm cup of tea together.")
+else:
+    print("Can you say that again?")
